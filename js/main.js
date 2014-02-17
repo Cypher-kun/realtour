4315dbcb5f76904ef148c494acfff4337b63e289
Anton Shchukin <cypher76@gmail.com>
2014-02-17 22:08:43 +0700

    Added touch support to work time

--- js/main.js
+++ js/main.js
@@ -5,3 +5,14 @@
 $(document).on('ready', function(){
     $('.no-placeholder input[placeholder], .no-placeholder textarea[placeholder]').placeholder();
 });
+
+
+/*
+ * Work time touch
+ */
+
+$(document).on('ready', function(){
+    $('.touch .work-time__week__item').on('click', function(){
+        $(this).find('.work-time__week__item__time').fadeToggle();
+    });
+});
