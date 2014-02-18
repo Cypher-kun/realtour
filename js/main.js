4088676a08922a79d7706a6ab25a6c4996d21c18
Anton Shchukin <cypher76@gmail.com>
2014-02-18 19:49:33 +0700

    Marked up feedback details page

--- js/main.js
+++ js/main.js
@@ -32,3 +32,66 @@ $(document).on('ready', function(){
     })(jQuery);
 
 });
+
+
+/* sticky sidebar */
+jQuery(document).ready(function ($) {
+
+    var $sidebarSlot  = $('.sidebar__item_sticky');
+    var $sidebarBlock = $('.sidebar__item__detachable');
+
+    var stickyPosition;
+    var windowScrolled;
+    var isSticked;
+    var isWideEnough;
+
+    function getIsWideEnough() {
+        return $(window).width() > 1000 ? true : false;
+    }
+
+    function stickSidebar() {
+        isSticked = true;
+        $sidebarBlock.addClass('sidebar__item__detachable_sticked');
+    }
+
+    function unstickSidebar() {
+        isSticked = false;
+        $sidebarBlock.removeClass('sidebar__item__detachable_sticked');
+    }
+
+
+    function sidebarStickingByWindowScroll() {
+        if( isWideEnough ) {
+            windowScrolled = $(window).scrollTop();
+            if ( ! isSticked && windowScrolled >= stickyPosition) stickSidebar();
+            if (   isSticked && windowScrolled <  stickyPosition) unstickSidebar();
+        }
+    }
+
+    function sidebarStickingByWindowResize() {
+        stickyPosition = $sidebarSlot.offset().top;
+        isWideEnough = getIsWideEnough();
+
+        if( isWideEnough ) {
+            sidebarStickingByWindowScroll();
+        }
+        else if( isSticked ) {
+            unstickSidebar();
+        }
+
+    }
+
+
+    function init() {
+        stickyPosition = $sidebarSlot.offset().top;
+        windowScrolled = $(window).scrollTop();
+        isWideEnough = getIsWideEnough();
+        isSticked = false;
+        sidebarStickingByWindowScroll();
+    }
+
+    $(window).on('scroll', sidebarStickingByWindowScroll );
+    $(window).on('resize', sidebarStickingByWindowResize );
+    $(window).on('load', init );
+
+});
